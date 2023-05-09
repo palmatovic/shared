74606668f6035c39821e8c7ed31840f5d90833e6
palmatovic <matteo.marucci@telsy.it>
2023-05-09 09:37:21 +0200

    Update

--- conf/service-configuration.go
+++ conf/service-configuration.go
@@ -1,81 +1,182 @@
 package conf
 
 import (
+	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/antihax/optional"
+	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/go-playground/validator/v10"
+	"github.com/go-sql-driver/mysql"
 	"github.com/palmatovic/shared/util"
+	sql "gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"net"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
-type ServiceConfiguration struct {
-	App   *App   `json:"app" validate:"required"`
-	Infra *Infra `json:"infra" validate:"required"`
+type serviceConfiguration struct {
+	app   *app   `json:"app" validate:"required"`
+	infra *infra `json:"infra" validate:"required"`
 }
 
-type App struct {
-	Port    *int    `json:"port" validate:"omitempty,gt=0"`
-	Name    *string `json:"name" validate:"omitempty,min=5"`
-	BaseUrl *string `json:"url" validate:"omitempty,url"`
+type app struct {
+	port    *int    `json:"port" validate:"omitempty,gt=0"`
+	name    *string `json:"name" validate:"omitempty,min=5"`
+	baseurl *string `json:"url" validate:"omitempty,url"`
 }
 
-type Infra struct {
-	Database *Database `json:"database" validate:"omitempty"`
-	Elastic  *Elastic  `json:"elastic" validate:"omitempty"`
+type infra struct {
+	database *client  `json:"database" validate:"omitempty"`
+	elastic  *client `json:"elastic" validate:"omitempty"`
 }
 
-type Elastic struct {
-	Host           string `json:"host" validate:"required,url"`
-	Port           int    `json:"port" validate:"required,gt=0"`
-	Username       string `json:"username" validate:"omitempty,min=5"`
-	Password       string `json:"password" validate:"omitempty,min=5"`
-	SSLCertificate string `json:"ssl_certificate" validate:"omitempty,required_if=UseSSL true"`
-	UseSSL         string `json:"use_ssl" validate:"omitempty,min=5"`
+
+func (c client) getElasticClient() (*elasticsearch.Client, error) {
+
+	var cfg elasticsearch.Config
+	pwd, err := c.getPassword()
+	if err!=nil {return nil, err}
+	cfg = elasticsearch.Config{
+		Username:  c.username,
+		Password:  pwd,
+		Addresses: []string{
+			func() string{
+				a := fmt.Sprintf("https://%s:%d",c.host,c.port)
+				if c.useSSL{
+					a = strings.ReplaceAll(a,"http","https")
+				}
+				return a
+			}(),
+		},
+		Transport: &http.Transport{
+			MaxIdleConnsPerHost:   10,
+			ResponseHeaderTimeout: time.Second,
+			DialContext:           (&net.Dialer{Timeout: time.Second}).DialContext,
+		},
+	}
+	if c.useSSL {
+		certificate, err := c.getSSLCertificate()
+		if err != nil {
+			return nil, err
+		}
+		cfg.CACert = certificate
+		cfg.Transport = &http.Transport{
+			MaxIdleConnsPerHost:   10,
+			ResponseHeaderTimeout: time.Second,
+			DialContext:           (&net.Dialer{Timeout: time.Second}).DialContext,
+			TLSClientConfig: &tls.Config{
+				MinVersion:         tls.VersionTLS12,
+				InsecureSkipVerify: c.InsecureSkipVerify,
+			},
+		}
+	}
+	if elasticClient, err = elasticsearch.NewClient(cfg); err != nil {
+		return nil, err
+	}
+	if _, err = elasticClient.Info(); err != nil {
+		return nil, err
+	}
+
+	return elasticClient, nil
+
 }
 
-type Database struct {
-	Host                          string `json:"host" validate:"omitempty,min=5"`
-	Port                          int    `json:"port" validate:"omitempty,min=5"`
-	Username                      string `json:"username" validate:"omitempty,min=5"`
-	PasswordFilepath              string `json:"password_filepath" validate:"omitempty,min=5"`
-	SSLCertificateFilepath        string `json:"ssl_certificate_filepath" validate:"omitempty,required_if=UseSSL true"`
-	UseSSL                        bool   `json:"use_ssl" validate:"required"`
-	Debug                         bool   `json:"debug" validate:"required"`
-	Automigrate                   bool   `json:"automigrate" validate:"required"`
-	DatabaseName                  string `json:"database_name" validate:"omitempty"`
-	UseEncryptedPassword          bool   `json:"use_encrypted_password" validate:"required"`
-	PasswordEncryptionKeyFilepath string `json:"password_encryption_key_filepath" validate:"required_if=UseEncryptedPassword true"`
+type client struct {
+	host                          string `json:"host" validate:"omitempty,min=5"`
+	port                          int    `json:"port" validate:"omitempty,min=5"`
+	username                      string `json:"username" validate:"omitempty,min=5"`
+	passwordFilepath              string `json:"password_filepath" validate:"omitempty,min=5"`
+	sslCertificateFilepath        string `json:"ssl_certificate_filepath" validate:"omitempty,required_if=UseSSL true"`
+	useSSL                        bool   `json:"use_ssl" validate:"required"`
+	debug                         bool   `json:"debug" validate:"required"`
+	automigrate                   bool   `json:"automigrate" validate:"required"`
+	databaseName                  string `json:"database_name" validate:"omitempty"`
+	useEncryptedPassword          bool   `json:"use_encrypted_password" validate:"required"`
+	passwordEncryptionKeyFilepath string `json:"password_encryption_key_filepath" validate:"required_if=UseEncryptedPassword true"`
+	skipSslVerification            bool  `json:"skipSslVerification" validate:"required_if=UseEncryptedPassword true"`
 }
 
-//
-//func (Database) GetClient() *gorm.DB {
-//
-//}
-//
-//func (d Database) GetAddress() string {
-//
-//}
+func (c client) getDatabaseClient(dbTables []interface{}) (db *gorm.DB, err error) {
+	var typeTLS string
+	if c.useSSL {
+		typeTLS = "custom"
+	} else {
+		typeTLS = optional.EmptyString().Value()
+	}
+	pwd, err := c.getPassword()
+	if err != nil {
+		return nil, err
+	}
+	configDB := mysql.Config{
+		User:                 c.username,
+		Passwd:               pwd,
+		Addr:                 c.GetAddress(),
+		Net:                  "tcp",
+		DBName:               c.databaseName,
+		Loc:                  time.UTC,
+		ParseTime:            true,
+		AllowNativePasswords: true,
+		TLSConfig:            typeTLS,
+	}
 
-func (d Database) GetPassword() (string, error) {
-	if d.UseEncryptedPassword {
-		pwdEncryptBytes, err := os.ReadFile(d.PasswordEncryptionKeyFilepath)
+	connectionString := configDB.FormatDSN()
+
+	if c.useSSL {
+		sslCert, err := c.getSSLCertificate()
+		if err != nil {
+			return nil, err
+		}
+		err = mysql.RegisterTLSConfig(typeTLS, &tls.Config{
+			RootCAs:    sslCert,
+			MinVersion: tls.VersionTLS12,
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+	db, err = gorm.Open(sql.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	if c.debug {
+		db = db.Debug()
+	}
+
+	if c.automigrate {
+		err = db.AutoMigrate(db, dbTables)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return db, nil
+}
+
+func (c client) getPassword() (string, error) {
+	if c.useEncryptedPassword {
+		pwdEncryptBytes, err := os.ReadFile(c.passwordEncryptionKeyFilepath)
 		if err != nil {
 			return "", err
 		}
-		return util.ReadEncryptedPasswordFromFile(d.PasswordFilepath, pwdEncryptBytes)
+		return util.ReadEncryptedPasswordFromFile(c.passwordFilepath, pwdEncryptBytes)
 	}
-	pwdBytes, err := os.ReadFile(d.PasswordFilepath)
+	pwdBytes, err := os.ReadFile(c.passwordFilepath)
 	if err != nil {
 		return "", err
 	}
 	return string(pwdBytes), nil
-
 }
 
-func (d Database) GetSSLCertificate() (*x509.CertPool, error) {
-	fileCA := d.SSLCertificateFilepath
+func (c client) getSSLCertificate() (*x509.CertPool, error) {
+	fileCA := c.sslCertificateFilepath
 	rootCertPool := x509.NewCertPool()
 	CA, err := os.ReadFile(fileCA)
 	if err != nil {
@@ -87,11 +188,21 @@ func (d Database) GetSSLCertificate() (*x509.CertPool, error) {
 	return rootCertPool, nil
 }
 
-func (d Database) GetAddress() string {
-	return fmt.Sprintf("%s:%d", d.Host, d.Port)
+func (c client) GetDatabaseAddress() string {
+	return fmt.Sprintf("%s:%c", c.host, c.port)
 }
 
-func LoadServiceConfiguration(configFilePath string) (*ServiceConfiguration, error) {
+type ConfigSetup struct {
+	Database struct {
+		Tables []interface{}
+	}
+}
+
+type Module struct {
+	Database *gorm.DB
+}
+
+func LoadServiceConfiguration(configFilePath string, setup ConfigSetup) (*Module, error) {
 	file, err := os.Open(configFilePath)
 	if err != nil {
 		return nil, fmt.Errorf("unable to open file %s: %w", configFilePath, err)
@@ -109,7 +220,7 @@ func LoadServiceConfiguration(configFilePath string) (*ServiceConfiguration, err
 	}
 	// Parse the JSON file into a ServiceConfiguration struct
 	decoder := json.NewDecoder(file)
-	config := new(ServiceConfiguration)
+	config := new(serviceConfiguration)
 
 	if err = decoder.Decode(config); err != nil {
 		return nil, fmt.Errorf("unable to parse file %s: %w", configFilePath, err)
@@ -119,5 +230,25 @@ func LoadServiceConfiguration(configFilePath string) (*ServiceConfiguration, err
 	if err := validate.Struct(config); err != nil {
 		return nil, fmt.Errorf("invalid configuration: %w", err)
 	}
-	return config, nil
+
+	// if exist database
+	module := Module{}
+
+	if config.infra.database != nil {
+		db, err := config.infra.database.getDatabaseClient(setup.Database.Tables)
+		if err != nil {
+			return nil, err
+		}
+		module.Database = db
+	}
+
+	if config.infra.elastic != nil {
+		db, err := config.infra.database.(setup.Database.Tables)
+		if err != nil {
+			return nil, err
+		}
+		module.Database = db
+	}
+
+	return module, nil
 }
--- mariadb/util-db.go
+++ mariadb/util-db.go
@@ -1,70 +1,7 @@
 package database
 
-import (
-	"crypto/tls"
-	"github.com/antihax/optional"
-	"github.com/go-sql-driver/mysql"
-	"github.com/palmatovic/shared/conf"
-	"time"
-
-	sql "gorm.io/driver/mysql"
-	"gorm.io/gorm"
-)
-
 // GetDB open new connection pool.
 // This method has to be invoked only once, maybe you have to make some tuning for pool size
-func GetDB(databaseConfig *conf.Database, dbTables ...interface{}) (db *gorm.DB, err error) {
-	var typeTLS string
-	if databaseConfig.UseSSL {
-		typeTLS = "custom"
-	} else {
-		typeTLS = optional.EmptyString().Value()
-	}
-	pwd, err := databaseConfig.GetPassword()
-	if err != nil {
-		return nil, err
-	}
-	configDB := mysql.Config{
-		User:                 databaseConfig.Username,
-		Passwd:               pwd,
-		Addr:                 databaseConfig.GetAddress(),
-		Net:                  "tcp",
-		DBName:               databaseConfig.DatabaseName,
-		Loc:                  time.UTC,
-		ParseTime:            true,
-		AllowNativePasswords: true,
-		TLSConfig:            typeTLS,
-	}
-
-	connectionString := configDB.FormatDSN()
-
-	if databaseConfig.UseSSL {
-		err = mysql.RegisterTLSConfig(typeTLS, &tls.Config{
-			RootCAs:    databaseConfig.GetSSLCertificate(),
-			MinVersion: tls.VersionTLS12,
-		})
-		if err != nil {
-			return nil, err
-		}
-	}
-	db, err = gorm.Open(sql.Open(connectionString), &gorm.Config{})
-	if err != nil {
-		return nil, err
-	}
-
-	if databaseConfig.Debug {
-		db = db.Debug()
-	}
-
-	if databaseConfig.Automigrate {
-		err = db.AutoMigrate(db, dbTables)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return db, nil
-}
 
 //type Format int64
 
